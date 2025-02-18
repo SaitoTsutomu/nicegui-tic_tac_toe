3ef6aa0b902ccc5b48178fb56d3f6ad0e8e77613
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2025-02-18 20:28:55 +0900

    upd

--- src/nicegui_tic_tac_toe/__init__.py
+++ src/nicegui_tic_tac_toe/__init__.py
@@ -10,7 +10,7 @@ _package_metadata = metadata(str(__package__))
 __version__ = _package_metadata["Version"]
 __author__ = _package_metadata.get("Author-email", "")
 
-__all__ = ["Game", "__author__", "__version__", "main"]
+__all__ = ["Game", "__author__", "__version__", "game"]
 
 
 def main() -> None:
